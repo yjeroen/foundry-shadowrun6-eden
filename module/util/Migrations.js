22cca24abf7265f040145757b20ec768123f6a5e
yeroon <jeroendenhaan@yahoo.com>
2025-05-05 09:04:01 +0200

    Improved migration check

--- module/util/Migrations.js
+++ module/util/Migrations.js
@@ -34,7 +34,7 @@ function migrateChatMessage(chatMessage) {
         };
 
         // Migrate dice css from individual badged-images to css badges
-        if(chatMessage.rolls) {
+        if(chatMessage.rolls.length) {
             chatMessage.rolls[0]?.results?.forEach(result => {
                 result.classes = result.classes.replace(/(die_[1-6])_exploded_ignored/, "$1 exploded ignored");
                 result.classes = result.classes.replace(/(die_[1-6])_exploded/, "$1 exploded");
@@ -46,5 +46,6 @@ function migrateChatMessage(chatMessage) {
         }
 
         chatMessage.update(updatedMsg);
+        console.log("SR6E | Migration | migrateChatMessage", chatMessage.id)
     }
 }
