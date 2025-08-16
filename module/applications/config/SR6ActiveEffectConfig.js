3787b73b0b8ba25058b5ca68d5613a7025a4d865
yjeroen <jeroendenhaan@yahoo.com>
2025-08-16 21:01:48 +0200

    AE Config window closes on Submit

--- module/applications/config/SR6ActiveEffectConfig.js
+++ module/applications/config/SR6ActiveEffectConfig.js
@@ -15,9 +15,7 @@ export default class SR6ActiveEffectConfig extends ActiveEffectConfig {
         return foundry.utils.mergeObject(super.defaultOptions, {
             template: "systems/shadowrun6-eden/templates/sheets/active-effect/v12-config.html",
             tabs: [{navSelector: ".tabs", contentSelector: "form", initial: "effects"}],
-            width: 700,
-            submitOnChange: true,
-            closeOnSubmit: false
+            width: 700
         });
     }
 
